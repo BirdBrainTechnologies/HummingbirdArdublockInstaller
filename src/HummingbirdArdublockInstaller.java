91aaeb58f68aa4d4558c73e208c1cc455230f8f6
birdbraintech <tlauwers@birdbraintechnologies.com>
2015-05-06 17:18:01 -0400

    Made installer more flexible
    Tries a few techniques to find the sketchbook directory, and if all
    fails it asks the user to find it.

--- src/HummingbirdArdublockInstaller.java
+++ src/HummingbirdArdublockInstaller.java
@@ -14,6 +14,9 @@ public class HummingbirdArdublockInstaller extends JFrame {
     private JButton goButton;
     private JPanel MainPanel;
     private JCheckBox AdvancedArdublock;
+    private JLabel helpText;
+    private JLabel installHelpText;
+    private JFileChooser fc;
 
     public HummingbirdArdublockInstaller(){
         goButton.addActionListener(new ActionListener() {
@@ -25,43 +28,64 @@ public class HummingbirdArdublockInstaller extends JFrame {
                 }
                 else if(SystemUtils.IS_OS_WINDOWS) {
                     prefs= new File(System.getenv("APPDATA")+"/Arduino/preferences.txt");
-                }
-                else{
+                    if(!prefs.exists()) {
+                        prefs = new File(com.sun.jna.platform.win32.Shell32Util.getFolderPath(0x001a) + "/Arduino/preferences.txt");
+                    }
+                } else {
                     prefs= new File(System.getProperty("user.home")+"/Library/Arduino/preferences.txt");
                 }
                 Hashtable table = new Hashtable();
                 try {
-                    load(new FileInputStream(prefs),table);
-                    String sketchbook= (String) table.get("sketchbook.path");
-                    FileUtils.copyDirectory(new File("files/Hummingbird"),new File(sketchbook+"/libraries/Hummingbird"));
-                    FileUtils.copyDirectory(new File("files/hardware/hbduo"),new File(sketchbook+"/hardware/hbduo"));
-                    if(AdvancedArdublock.isSelected()) {
-                        FileUtils.copyFileToDirectory(new File("files/Ardublock_advanced/ardublock-all.jar"), new File(sketchbook + "/tools/ArduBlockTool/tool"));
+                    String sketchbook = null;
+                    String message;
+                    if(prefs.exists()) {
+                        load(new FileInputStream(prefs), table);
+                        sketchbook = (String) table.get("sketchbook.path");
                     }
                     else {
-                        FileUtils.copyFileToDirectory(new File("files/Ardublock_normal/ardublock-all.jar"), new File(sketchbook + "/tools/ArduBlockTool/tool"));
+                        fc = new JFileChooser();
+                        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+                        System.out.println(fc.getX() + " " + fc.getY() + " " + MainPanel.getX() + " " + MainPanel.getY());
+                        JOptionPane.showMessageDialog(null,"Couldn't automatically find the Arduino Sketchbook directory, press OK to manually select it.\n If unsure of the location, open the Arduino IDE, select File->Preferences and note the \"Sketchbook\" location.");
+                        int returnVal = fc.showDialog(HummingbirdArdublockInstaller.this, "Select");
+                        if(returnVal == JFileChooser.APPROVE_OPTION) {
+                            sketchbook = fc.getSelectedFile().toString();
+                            installHelpText.setText("Files will be installed to " + sketchbook);
+                        }
                     }
 
-                    FileUtils.copyDirectory(new File("files/ArdublockDuoExamples"),new File(sketchbook+"/ArdublockDuoExamples"));
+                    if(sketchbook != null) {
 
-                    String message;
-                    if(SystemUtils.IS_OS_WINDOWS) {
-                        Process p = Runtime.getRuntime().exec("files\\WindowsDrivers\\runDriverInstall.bat");
-                        message = "Done copying files, now installing Duo driver";
+                        FileUtils.copyDirectory(new File("files/Hummingbird"), new File(sketchbook + "/libraries/Hummingbird"));
+                        FileUtils.copyDirectory(new File("files/hardware/hbduo"), new File(sketchbook + "/hardware/hbduo"));
+                        if (AdvancedArdublock.isSelected()) {
+                            FileUtils.copyFileToDirectory(new File("files/Ardublock_advanced/ardublock-all.jar"), new File(sketchbook + "/tools/ArduBlockTool/tool"));
+                        } else {
+                            FileUtils.copyFileToDirectory(new File("files/Ardublock_normal/ardublock-all.jar"), new File(sketchbook + "/tools/ArduBlockTool/tool"));
+                        }
+
+                        FileUtils.copyDirectory(new File("files/ArdublockDuoExamples"), new File(sketchbook + "/ArdublockDuoExamples"));
+
+                        if (SystemUtils.IS_OS_WINDOWS) {
+                            Process p = Runtime.getRuntime().exec("files\\WindowsDrivers\\runDriverInstall.bat");
+                            message = "Done copying files, now installing Duo driver";
+                        } else {
+                            message = "Done! Close me and restart the Arduino IDE!";
+                        }
                     }
-                    else
-                    {
-                        message = "Done!";
+                    else {
+                        message = "No Arduino Sketchbook directory found, please install the Arduino environment and run it once, then try this installer again.";
                     }
                     JOptionPane.showMessageDialog(null,message);
                 }catch(Exception e){
-                    JOptionPane.showMessageDialog(null,"An error occurred. Please try again.");
+                    JOptionPane.showMessageDialog(null,"An error occurred. Please try again or use the manual way to install described in the tutorial.");
                     e.printStackTrace();
                 }
             }
         });
         setContentPane(MainPanel);
-        setTitle("Hummingbird + Ardublock Installer");
+        setTitle("Hummingbird Arduino Library Installer");
         setLocation(100, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
@@ -127,5 +151,9 @@ public class HummingbirdArdublockInstaller extends JFrame {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
